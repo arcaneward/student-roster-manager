0a21b955725c074460884389146b03c8f46cb174
Wendy <84740121+we-ndy@users.noreply.github.com>
2021-10-12 17:48:14 -0500

    Update roster.h

--- roster.h
+++ roster.h
@@ -1,1 +1,32 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "degree.h"
+#include "student.h"
 
+using namespace std;
+
+// Requirement E
+class Roster {
+
+    private:
+        Student* classRosterArray[5];
+        string studentID;
+        string firstName;
+        string lastName;
+        string email;
+        int age;
+        int daysInCourse1;
+        int daysInCourse2;
+        int daysInCourse3;
+        DegreeProgram degProgram;
+    
+    public:
+        void parseAdd();
+        void remove(string studID);
+        void PrintRoster() const;
+        void printAvgDaysInCourse(string studID);
+        void printInvalidEmailAddress() const;
+        void printByDegree(DegreeProgram degreeprogram);
+        Roster();
+        ~Roster(); };
