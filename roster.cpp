d5e6dd62e7f6c7360b35390594d363b0b7d61e57
L <84740121+lin-dsey@users.noreply.github.com>
2021-11-05 12:11:27 -0500

    Update roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -1,4 +1,4 @@
-#include<string>
+t#include<string>
 #include<iostream>
 #include "roster.h"
 
@@ -9,7 +9,7 @@ using namespace std;
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,Lindsey,Thornton,lthor71@wgu.edu,35,21,33,49,SOFTWARE" }; 
+        "A5,Redacted,Redacted,redacted@school.edu,35,21,33,49,SOFTWARE" }; 
 
     // Requirement E
     void Roster::parseAdd() {
