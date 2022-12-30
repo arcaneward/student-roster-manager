f902ce48167629746a54b08c81330563a17be9e7
Lindsey <84740121+lin-dsey@users.noreply.github.com>
2022-12-30 13:57:24 -0600

    Update roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -5,10 +5,10 @@ t#include<string>
 using namespace std;
     // Requirement A
     const string studentData[] =
-        {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        {"A1,Yoongi,Min,yoongi@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Kim,Namjoon,knamjoon@gmailcom,19,50,30,40,NETWORK",
+        "A3,Kim,Napoli,Taeyhung99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Kimseok,Jin,Erin.black@comcast.net,22,50,58,40,SECURITY",
         "A5,Redacted,Redacted,redacted@school.edu,35,21,33,49,SOFTWARE"}; 
 
     // Requirement E
